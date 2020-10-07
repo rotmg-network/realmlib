facb05c0c1b8b3b693b760ba18d08a1656db7f40
x <x@invalid.com>
2020-10-07 19:31:22 +0100

    Update show-effect-packet.ts

--- src/packets/incoming/show-effect-packet.ts
+++ src/packets/incoming/show-effect-packet.ts
@@ -3,6 +3,7 @@ import { Packet } from '../../packet';
 import { PacketType } from '../../packet-type';
 import { Reader } from '../../reader';
 import { Writer } from '../../writer';
+import { read as compressedRead } from '../../data/compressed-int';
 
 /**
  * Received to tell the player to display an effect such as an AOE grenade.
@@ -49,11 +50,42 @@ export class ShowEffectPacket implements Packet {
 
   read(reader: Reader): void {
     this.effectType = reader.readUnsignedByte();
-    this.targetObjectId = reader.readInt32();
-    this.pos1.read(reader);
-    this.pos2.read(reader);
-    this.color = reader.readInt32();
-    this.duration = reader.readFloat();
+    let loc2 = reader.readUnsignedByte();
+    if (loc2 & 64) {
+      this.targetObjectId = compressedRead(reader);
+    } else {
+      this.targetObjectId = 0;
+    }
+    if(loc2 & 2){
+      this.pos1.x = reader.readFloat();
+    } else {
+      this.pos1.x = 0;
+    }
+    if(loc2 & 4) {
+            this.pos1.y = reader.readFloat();
+    } else {
+      this.pos1.y = 0;
+    }
+    if(loc2 & 8){
+      this.pos2.x = reader.readFloat();
+    } else {
+      this.pos2.x = 0;
+    }
+    if(loc2 & 16) {
+      this.pos2.y = reader.readFloat();
+    } else {
+      this.pos2.y = 0;
+    }
+    if(loc2 & 1) {
+      this.color = reader.readInt32();
+    } else {
+      this.color = 4294967295;
+    }
+    if(loc2 & 32) {
+      this.duration = reader.readFloat();
+    } else {
+      this.duration = 1;
+    }
   }
 
   write(writer: Writer): void {
