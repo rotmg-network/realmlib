a3430bcb62039abff38c09586896f5ba8b99ff49
x <x@invalid.com>
2020-10-07 19:31:18 +0100

    add queue info packet

---
+++ src/packets/incoming/queue-info.ts
@@ -0,0 +1,37 @@
+import { Packet } from '../../packet';
+import { PacketType } from '../../packet-type';
+import { Reader } from '../../reader';
+import { Writer } from '../../writer';
+
+/**
+ * Received when the client connects to a server with a queue.
+ */
+export class QueueInfoPacket implements Packet {
+    readonly type = PacketType.QUEUE_INFO;
+
+    /**
+     * The current position of the client in the queue
+     */
+    currentPosition: number;
+
+    /**
+     * The maximum amount of clients allowed in the queue
+     */
+    maxPosition: number;
+
+    constructor()
+    {
+        this.currentPosition = 0;
+        this.maxPosition = 0;
+    }
+
+    read(reader: Reader): void {
+        this.currentPosition = reader.readUnsignedShort();
+        this.maxPosition = reader.readUnsignedShort();
+    }
+
+    write(writer: Writer): void {
+        writer.writeUnsignedShort(this.currentPosition);
+        writer.writeUnsignedShort(this.maxPosition)
+    }
+}
