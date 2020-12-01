7c8b32a0d46b04c03395afea00ef1a924f5db4ef
abrn <lordzhhao@gmail.com>
2020-12-01 22:07:35 +0000

    add exaltation update packet

--- src/packets/incoming/vault-update.ts
+++ src/packets/incoming/vault-update.ts
@@ -9,19 +9,33 @@ import { Writer } from "../../writer";
  */
 export class VaultUpdatePacket implements Packet {
   readonly type = PacketType.VAULT_UPDATE;
-
+  /**
+   * The contents of the players vault, sent as an array of item object IDs or -1 if the slot is empty
+   */
   vaultContents: number[];
-
+  /**
+   * The contents of the player's gift vault
+   */
   giftContents: number[];
-
+  /**
+   * The contents of the player's potion vault
+   */
   potionContents: number[];
-
+  /**
+   * The cost in gold for the next upgrade to the vault
+   */
   vaultUpgradeCost: number;
-
+  /**
+   * The cost in gold for the next upgrade to the potion vault
+   */
   potionUpgradeCost: number;
-
+  /**
+   * The current slot size of the player's potion vault
+   */
   currentPotionMax: number;
-
+  /**
+   * The size of the player's potion vault after they purchase the current upgrade
+   */
   nextPotionMax: number;
 
   constructor() {
@@ -61,7 +75,7 @@ export class VaultUpdatePacket implements Packet {
       this.potionContents.push(compressedRead(reader));
       counter++;
     }
-    
+
     this.vaultUpgradeCost = reader.readShort();
     this.potionUpgradeCost = reader.readShort();
     this.currentPotionMax = reader.readShort();
