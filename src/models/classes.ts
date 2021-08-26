94b0cc09f66200ef61a236d3f856246995bb97df
him <im@him.is>
2021-08-26 21:47:46 +0100

    add Summoner class and function for parsing class IDs

--- src/models/classes.ts
+++ src/models/classes.ts
@@ -17,5 +17,33 @@ export declare enum Classes {
     Sorcerer = 805,
     Ninja = 806,
     Samurai = 785,
-    Bard = 796
+    Bard = 796,
+    Summoner = 817
+}
+
+/**
+ * Return the name of a class given the numerical ID
+ * @param classId The class ID
+ */
+export function parsePlayerClass(classId: number): string {
+    switch(classId) {
+        case Classes.Rogue: return "Rogue"
+        case Classes.Archer: return "Archer"
+        case Classes.Wizard: return "Wizard"
+        case Classes.Priest: return "Priest"
+        case Classes.Warrior: return "Warrior"
+        case Classes.Knight: return "Knight"
+        case Classes.Paladin: return "Paladin"
+        case Classes.Assassin: return "Assassin"
+        case Classes.Necromancer: return "Necromancer"
+        case Classes.Huntress: return "Huntress"
+        case Classes.Mystic: return "Mystic"
+        case Classes.Trickster: return "Trickster"
+        case Classes.Sorcerer: return "Sorcerer"
+        case Classes.Ninja: return "Ninja"
+        case Classes.Samurai: return "Samurai"
+        case Classes.Bard: return "Bard"
+        case Classes.Summoner: return "Summoner"
+        default: return "Unknown"
+    }
 }
