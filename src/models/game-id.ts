c874789138a65961c9574e5cafc2e3356187036a
him <im@him.is>
2026-07-06 05:45:22 +0100

    Add GameId constants for different map IDs

---
+++ src/models/game-id.ts
@@ -0,0 +1,24 @@
+/**
+ * Known map ids accepted in the Hello packet. Negative ids are special maps;
+ * realm instances and portal reconnects use server-assigned ids.
+ */
+export enum GameId {
+  Tutorial = -1,
+  Nexus = -2,
+  RandomRealm = -3,
+  NexusTutorial = -4,
+  Vault = -5,
+  MapTest = -6,
+  VaultExplanation = -8,
+  NexusExplanation = -9,
+  QuestRoom = -11,
+  CheatersQuarantine = -13,
+}
+
+/**
+ * Backwards-compatible aliases for older client code.
+ */
+export const GAME_ID = {
+  NEXUS: GameId.Nexus,
+  TUTORIAL: GameId.Tutorial,
+};
